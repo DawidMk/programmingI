a6b88a039b43296d38ab0352f09ac34dabe2091d
DawidMk <dawid_m@interia.pl>
2019-02-11 20:11:27 +0100

    added stream versions of methods for animals and owners

---
+++ src/main/java/pl/sda/programmingI/day4/animalAndOwner/Main.java
@@ -0,0 +1,122 @@
+package pl.sda.programmingI.day4.animalAndOwner;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+public class Main {
+    public static List<Food> allFoodList = new ArrayList<>();
+    public static List<Person> personList = new ArrayList<>();
+    //    public static List<Animal> animalList = new ArrayList<>();
+    public static Random random = new Random();
+
+    public static void main(String[] args) {
+        populateFoodList(50);
+        populatePersonList(10);
+
+        List<Animal> list = animalListStream();
+        for (Animal animal : list) {
+            System.out.println(animal.getName());
+            for (Food food : animal.getFavFood()) {
+                System.out.println(food.getName());
+            }
+            System.out.println("koniec zwierza");
+
+        }
+/*
+        for (Person person : personList) {
+            System.out.println(person.getName());
+            System.out.println(person.getAnimalList().toString());
+            System.out.println(person.getFoodList().toString());
+            System.out.println();
+        }*/
+    }
+
+    // stwórz listę zwierząt
+    private static List<Animal> animalList() {
+        List<Animal> animals = new ArrayList<>();
+        int size = random.nextInt(5);
+        for (int i = 0; i < size; i++) {
+            List<Food> list = genericPopulateFood(8);
+            Animal animal = new Animal("anim " + i, list);
+            animals.add(animal);
+        }
+        return animals;
+
+    }
+
+    // stwórz listę ludzi
+    private static void populatePersonList(int size) {
+        for (int i = 0; i < size; i++) {
+            Person person = new Person("person " + i, populatePersonalFood(), animalList());
+            personList.add(person);
+        }
+    }
+
+    //stwórz listę wszystkich rodzajów jedzenia 'allFoodList'
+    private static void populateFoodList(int size) {
+        for (int i = 1; i <= size; i++) {
+            allFoodList.add(new Food("food " + i));
+        }
+    }
+
+    //stwórz listę jedzenia, które ma człowiek; niepotrzebna metoda, może być zastąpiona przez genericPopulateFood
+    private static List<Food> populatePersonalFood() {
+        int foodCount = random.nextInt(5);
+        List<Food> foodList = new ArrayList<>();
+        for (int i = 0; i < foodCount; i++) {
+            foodList.add(allFoodList.get(random.nextInt(allFoodList.size())));
+        }
+        return foodList;
+    }
+
+    //stwórz listę ulubionego jedzenia zwierza; niepotrzebna metoda, może być zastąpiona przez genericPopulateFood
+    private static List<Food> populateFavFood() {
+        int foodCount = random.nextInt(8);
+        List<Food> foodList = new ArrayList<>();
+        for (int i = 0; i < foodCount; i++) {
+            foodList.add(allFoodList.get(random.nextInt(allFoodList.size())));
+        }
+        return foodList;
+    }
+
+    //ogólna metoda tworząca listę jedzenia
+    private static List<Food> genericPopulateFood(int length) {
+        int foodCount = random.nextInt(length);
+        List<Food> foodList = new ArrayList<>();
+        for (int i = 0; i < foodCount; i++) {
+            foodList.add(allFoodList.get(random.nextInt(allFoodList.size())));
+        }
+        return foodList;
+    }
+
+
+    //=================================================streams====================================================================
+
+    //ogólna metoda tworząca listę jedzenia
+    private static List<Food> genericPopulateFoodStream(int length) {
+        int size = random.nextInt(length);
+        return IntStream.range(0, size)
+                .map(i -> random.nextInt(allFoodList.size()))
+                .mapToObj(allFoodList::get)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Animal> animalListStream() {
+        int size = random.nextInt(5);
+        return IntStream.range(0, size)
+                .mapToObj(i -> new Animal("anim" + i, genericPopulateFoodStream(8)))
+                .collect(Collectors.toList());
+    }
+
+    private static List<Person> populatePersonListStream(int size) {
+        return IntStream.range(0, size)
+                .mapToObj(i -> new Person("name " + i, genericPopulateFood(3), animalListStream()))
+                .collect(Collectors.toList());
+    }
+
+}
