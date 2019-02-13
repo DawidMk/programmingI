c8d587e849be649a2a5447498ea8ecf43063a017
DawidMk <dawid_m@interia.pl>
2019-02-13 18:38:28 +0100

    populatePersonListStream v 1

--- src/main/java/pl/sda/programmingI/day4/animalAndOwner/Main.java
+++ src/main/java/pl/sda/programmingI/day4/animalAndOwner/Main.java
@@ -2,6 +2,7 @@ package pl.sda.programmingI.day4.animalAndOwner;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -11,6 +12,7 @@ import java.util.stream.IntStream;
 public class Main {
     public static List<Food> allFoodList = new ArrayList<>();
     public static List<Person> personList = new ArrayList<>();
+    public static List<Person> personListTest = new ArrayList<>();
     //    public static List<Animal> animalList = new ArrayList<>();
     public static Random random = new Random();
 
@@ -21,24 +23,29 @@ public class Main {
         personList = populatePersonListStream(10);
 
         List<Animal> list = animalListStream();
-        System.out.println("animal list test");
+//        System.out.println("animal list test");
+
+/*
         for (Animal animal : list) {
             System.out.println(animal.getName());
+            System.out.println(animal.getOwner());
             for (Food food : animal.getFavFood()) {
                 System.out.println(food.getName());
             }
             System.out.println("koniec zwierza");
-        }
+        }*/
 
         System.out.println("\nperson list test");
-
-        List<Person> peopleList = populatePersonListStream(5);
-        for (Person person : peopleList) {
+        populatePersonList2(5);
+//        List<Person> peopleList = populatePersonList2(5);
+        for (Person person : personListTest) {
             System.out.println();
             System.out.println(person.getName());
             System.out.println("animals:");
             for (Animal animal : person.getAnimalList()) {
                 System.out.println(animal.getName());
+                System.out.println("OWNER");
+                System.out.println(animal.getOwner().getName());
             }
             System.out.println("food:");
             for (Food food : person.getFoodList()) {
@@ -73,6 +80,27 @@ public class Main {
             Person person = new Person("person " + i, populatePersonalFood(), animalList());
             personList.add(person);
         }
+    }
+
+    private static void populatePersonList2(int size) {
+        for (int i = 0; i < size; i++) {
+            Person person = new Person("person " + i, populatePersonalFood(), animalList());
+            for (Animal animal : person.getAnimalList()) {
+                animal.setOwner(person);
+            }
+            personListTest.add(person);
+        }
+    }
+
+    private static List<Person> populatePersonList2Stream(int size){
+        List<Person> list = new ArrayList<>();
+        return IntStream.range(0,size)
+                .mapToObj(i -> new Person("person "+i,populatePersonalFood(),animalList()))
+                .map(person -> person.getAnimalList().stream()
+                        .map(animal -> animal.setOwner(person)))
+                .collect(Collectors.toList());
+//       list.stream().map(person -> person.getAnimalList().stream().map(animal -> animal.setOwner(person))).collect(Collectors.toList());
+
     }
 
     //stwórz listę wszystkich rodzajów jedzenia 'allFoodList'
@@ -131,7 +159,8 @@ public class Main {
                 .mapToObj(i -> new Animal("anim" + i, genericPopulateFoodStream(8)))
                 .collect(Collectors.toList());
     }
-//stwórz listę ludzi
+
+    //stwórz listę ludzi
     private static List<Person> populatePersonListStream(int size) {
         return IntStream.range(0, size)
                 .mapToObj(i -> new Person("person " + i, genericPopulateFood(3), animalListStream()))
