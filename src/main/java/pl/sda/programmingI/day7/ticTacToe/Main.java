329003a4cc2b15ed133d9a35622f9695242d6df1
DawidMk <dawid_m@interia.pl>
2019-02-25 19:16:37 +0100

    updated tic tac toe

--- src/main/java/pl/sda/programmingI/day7/ticTacToe/Main.java
+++ src/main/java/pl/sda/programmingI/day7/ticTacToe/Main.java
@@ -69,51 +69,54 @@ public class Main {
     private static void takeField(int[] tab, boolean flag) {
         checkIfBoardIsFull();
         int x = chooseField();
-            int n = 0;
-            if (flag) {
-                n = 1;
-            } else {
-                n = 2;
-            }
-            if (tab[x] == 0) {
-                tab[x] = n;
-            } else {
-                System.out.println("field already taken. choose something else");
-                takeField(tab, flag);
-            }
-            showBoard(tab);
-            checkIfWin(tab, n);
+        int n = 0;
+        if (flag) {
+            n = 1;
+        } else {
+            n = 2;
         }
+        if (tab[x] == 0) {
+            tab[x] = n;
+        } else {
+            System.out.println("field already taken. choose something else");
+            takeField(tab, flag);
+        }
+        showBoard(tab);
+        checkIfWin(tab, n);
+    }
 
     private static void checkIfBoardIsFull() {
         if (Arrays.stream(gameBoard)
-                        .noneMatch(i -> i==0)
-                ) {
+                .noneMatch(i -> i == 0)
+        ) {
             System.out.println("board full! no winner this time!");
             System.exit(0);
         }
     }
 
-    private static int chooseField () {
-            System.out.println("which field? (0-8)");
-            String field = scanner.next();
-            boolean b2 = Pattern.compile("[0-8]").matcher(field).matches();
-            if (!b2 || Integer.valueOf(field) > 8) {
-                System.out.println("wrong input!");
-                chooseField();
-            }
-            return Integer.valueOf(field);
+    private static int chooseField() {
+        System.out.println("which field? (0-8)");
+        int output;
+        String field = scanner.next();
+        boolean b2 = Pattern.compile("[0-8]").matcher(field).matches();
+        if (!b2 || Integer.valueOf(field) > 8) {
+            System.out.println("wrong input!");
+            output = chooseField();
+        } else {
+            output = Integer.valueOf(field);
         }
-
-        private static void showBoard ( int[] tab){
-            System.out.print(tab[0] + "|" + tab[1] + "|" + tab[2]);
-            System.out.println();
-            System.out.print(tab[3] + "|" + tab[4] + "|" + tab[5]);
-            System.out.println();
-            System.out.print(tab[6] + "|" + tab[7] + "|" + tab[8]);
-            System.out.println();
-        }
+        return output;
     }
+
+    private static void showBoard(int[] tab) {
+        System.out.print(tab[0] + "|" + tab[1] + "|" + tab[2]);
+        System.out.println();
+        System.out.print(tab[3] + "|" + tab[4] + "|" + tab[5]);
+        System.out.println();
+        System.out.print(tab[6] + "|" + tab[7] + "|" + tab[8]);
+        System.out.println();
+    }
+}
 
 
 
